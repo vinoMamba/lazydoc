ecd891ddc7e7c8729942ba41c7b1e2da37dfb2f4
vino <vino0908@outlook.com>
2024-10-01 19:20:27 +0800

    feat: useDebounced

--- web/components/project/search-project-input.tsx
+++ web/components/project/search-project-input.tsx
@@ -2,13 +2,17 @@
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { Input } from "@/components/ui/input"
+import { Command } from "lucide-react"
+import { useEffect, useRef } from "react"
+import { useDebouncedCallback } from 'use-debounce';
 
 export const SearchInput = () => {
   const searchParams = useSearchParams()
   const pathname = usePathname()
   const { replace } = useRouter()
+  const inputRef = useRef<HTMLInputElement | null>(null)
 
-  const handleSearch = (name: string) => {
+  const handleSearch = useDebouncedCallback((name: string) => {
     const params = new URLSearchParams(searchParams)
     if (name) {
       params.set('name', name)
@@ -16,8 +20,33 @@ export const SearchInput = () => {
       params.delete('name')
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300)
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if ((event.metaKey && event.key === 'k') || (event.ctrlKey && event.key === 'k')) {
+      if (inputRef.current) {
+        inputRef.current.focus();
+      }
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [])
+
   return (
-    <Input placeholder="Search project" onChange={e => handleSearch(e.target.value)} />
+    <div className="w-full relative">
+      <Input
+        placeholder="Search project"
+        onChange={e => handleSearch(e.target.value)}
+        ref={inputRef}
+      />
+      <kbd className="absolute top-1/2 -translate-y-1/2 right-2 pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+        <Command className="w-[0.6rem] h-[0.6rem]" />K
+      </kbd>
+    </div>
   )
 }
--- web/components/user-table/search-input.tsx
+++ web/components/user-table/search-input.tsx
@@ -2,13 +2,14 @@
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { Input } from "@/components/ui/input"
+import { useDebouncedCallback } from 'use-debounce';
 
 export const SearchInput = () => {
   const searchParams = useSearchParams()
   const pathname = usePathname()
   const { replace } = useRouter()
 
-  const handleSearch = (username: string) => {
+  const handleSearch = useDebouncedCallback((username: string) => {
     const params = new URLSearchParams(searchParams)
     if (username) {
       params.set('condition', username)
@@ -17,7 +18,7 @@ export const SearchInput = () => {
     }
     params.set('pageNum', '1')
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300)
   return (
     <Input placeholder="Search user" onChange={e => handleSearch(e.target.value)} />
   )
